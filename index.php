4c361052fc49e64109a4f39d1c1caf081ff01a71
Dai Fahriyyan <dfahriyyan@gmail.com>
2021-01-20 21:54:17 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Git dan GitHub</title>
+</head>
+
+<body>
+    <h1>Hello World!!!</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis animi eius, dolor vero ab ipsum accusantium distinctio obcaecati officia laudantium, enim fugiat amet illum debitis, harum vitae sint. Culpa, error.</p>
+</body>
+
+</html>
